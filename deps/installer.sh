a94547827af17ce14d058fe417a5aff503c90e1a
Andrea Mangrella <mangrella.andrea@gmail.com>
2023-06-23 11:54:44 +0200

    Fixed the script

--- deps/installer.sh
+++ deps/installer.sh
@@ -1,15 +1,14 @@
-cd ..
-python3 -m venv ar_project
-source ar_project/bin/activate
-pip install -r ./deps/requirements.txt
-echo "Downloading Smtlib Inputs!\n"
+cd ..;
+python3 -m venv ar_project;
+source ar_project/bin/activate && pip install -r ./deps/requirements.txt;
+echo "Downloading Smtlib Inputs!\n";
 git clone https://github.com/kativenOG/ar_inputs.git;
 cd "./ar_inputs";
 N=$( ls | wc -l );
 cd ..;
-pyinstaller --onefile ../main.py --hidden-import="PIL._tkinter_finder"
-pyinstaller --onefile ../blessed_tui.py --hidden-import="PIL._tkinter_finder"
-rm -rf build main.spec blessed_tui.spec 
+# source ar_project/bin/activate && pyinstaller --onefile ./main.py --hidden-import="PIL._tkinter_finder";
+# source ar_project/bin/activate && pyinstaller --onefile ./blessed_tui.py --hidden-import="PIL._tkinter_finder";
+# rm -rf build main.spec blessed_tui.spec
 mkdir "outputs";
 for i in $(seq 1 $N);
 do
